5dc2dc41e8348707f8c192dfba0c74a0be3fa423
Dan Cervelli <dcervelli@mycompany.com>
2005-09-03 19:02:09 +0000

    Changes for Butterworth.
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3/trunk@109 b23e0666-5c4d-0410-a39a-a125d005d78a

--- gov/usgs/valve3/plotter/WavePlotter.java
+++ gov/usgs/valve3/plotter/WavePlotter.java
@@ -1,16 +1,23 @@
 package gov.usgs.valve3.plotter;
 
 import gov.usgs.math.Butterworth;
+import gov.usgs.math.FFT;
+import gov.usgs.math.Butterworth.FilterType;
+import gov.usgs.plot.Data;
+import gov.usgs.plot.DataRenderer;
 import gov.usgs.plot.Plot;
 import gov.usgs.util.Pool;
 import gov.usgs.valve3.PlotComponent;
 import gov.usgs.valve3.PlotHandler;
 import gov.usgs.valve3.Plotter;
 import gov.usgs.valve3.Valve3;
+import gov.usgs.valve3.Valve3Exception;
 import gov.usgs.valve3.result.Valve3Plot;
 import gov.usgs.vdx.client.VDXClient;
+import gov.usgs.vdx.data.wave.SliceWave;
 import gov.usgs.vdx.data.wave.Wave;
-import gov.usgs.vdx.data.wave.plot.WaveRenderer;
+import gov.usgs.vdx.data.wave.plot.SliceWaveRenderer;
+import gov.usgs.vdx.data.wave.plot.SpectrogramRenderer;
 
 import java.awt.Color;
 import java.io.File;
@@ -19,46 +26,169 @@ import java.util.HashMap;
 /**
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/08/26 20:41:31  dcervelli
+ * Initial avosouth commit.
+ *
  * @author Dan Cervelli
  */
 public class WavePlotter extends Plotter
 {
+	private enum PlotType
+	{
+		WAVEFORM, SPECTRA, SPECTROGRAM;
+		
+		public static PlotType fromString(String s)
+		{
+			if (s == null)
+				return null;
+			
+			if (s.equals("wf"))
+				return WAVEFORM;
+			else if (s.equals("sp"))
+				return SPECTRA;
+			else if (s.equals("sg"))
+				return SPECTROGRAM;
+			else 
+				return null;
+		}
+	}
+	
+	private Valve3Plot v3Plot;
+	private PlotComponent component;
+	private String channel;
+	private double startTime;
+	private double endTime;
+	private PlotType plotType;
+	private boolean removeBias;
+	private FilterType filterType;
+	private SliceWave wave;
+	private double minHz;
+	private double maxHz;
+	private double minFreq;
+	private double maxFreq;
+	
 	public WavePlotter()
 	{}
 
-	private void plotWaveform(Valve3Plot v3Plot, PlotComponent component, Wave w)
+	public void getData() throws Valve3Exception
 	{
-		boolean removeBias = component.get("rb").equals("T");
-		Wave wave = new Wave(w);
-		WaveRenderer wr = new WaveRenderer();
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("source", vdxSource);
+		params.put("selector", channel);
+		params.put("st", Double.toString(startTime));
+		params.put("et", Double.toString(endTime));
+
+		Pool<VDXClient> pool = Valve3.getInstance().getDataHandler().getVDXClient(vdxClient);
+		VDXClient client = pool.checkout();
+		Wave w = (Wave)client.getData(params);
+		pool.checkin(client);
+		
+		if (w == null)
+			throw new Valve3Exception("No data available for " + channel + ".");
+		
+		if (filterType != null)
+		{
+			Butterworth bw = new Butterworth();
+			switch(filterType)
+			{
+			case LOWPASS:
+				if (maxHz <= 0)
+					throw new Valve3Exception("Illegal max hertz value.");
+				bw.set(FilterType.LOWPASS, 4, w.getSamplingRate(), maxHz, 0);
+				w.filter(bw, true);
+				break;
+			case HIGHPASS:
+				if (minHz <= 0)
+					throw new Valve3Exception("Illegal minimum hertz value.");
+				bw.set(FilterType.HIGHPASS, 4, w.getSamplingRate(), minHz, 0);
+				w.filter(bw, true);
+				break;
+			case BANDPASS:
+				if (minHz <= 0 || maxHz <= 0 || minHz > maxHz)
+					throw new Valve3Exception("Illegal minimum/maximum hertz values.");
+				bw.set(FilterType.BANDPASS, 4, w.getSamplingRate(), minHz, maxHz);
+				w.filter(bw, true);
+				break;
+			}
+		}
+		
+		wave = new SliceWave(w);
+		wave.setSlice(startTime, endTime);
+	}
+	
+	public void getInputs() throws Valve3Exception
+	{
+		channel  = component.get("ch");
+		if (channel == null || channel.indexOf(";") != -1)
+			throw new Valve3Exception("Illegal channel name.");
+
+//		 TODO: move time checking to static method in Plotter
+		endTime = component.getEndTime();
+		if (Double.isNaN(endTime))
+			throw new Valve3Exception("Illegal end time.");
+		startTime = component.getStartTime(endTime);
+		if (Double.isNaN(startTime))
+			throw new Valve3Exception("Illegal start time.");
+
+		plotType = PlotType.fromString(component.get("type"));
+		if (plotType == null)
+			throw new Valve3Exception("Illegal plot type.");
+		
+		removeBias = false;
+		String clip = component.get("rb");
+		if (clip != null && clip.toUpperCase().equals("T"))
+			removeBias = true;
+		
+		filterType = FilterType.fromString(component.get("ftype"));
+		minHz = -1;
+		maxHz = -1;
+		try
+		{
+			minHz = Double.parseDouble(component.get("fminHz"));
+			maxHz = Double.parseDouble(component.get("fmaxHz"));
+		}
+		catch (Exception e) {}
+		
+		minFreq = -1;
+		maxFreq = -1;
+		try
+		{
+			minFreq = Double.parseDouble(component.get("spminf"));
+			maxFreq = Double.parseDouble(component.get("spmaxf"));
+		}
+		catch (Exception e) {}
+		
+		if (plotType == PlotType.SPECTRA || plotType == PlotType.SPECTROGRAM)
+		{
+			if (minFreq < 0 || maxFreq <= 0 || minFreq >= maxFreq)
+				throw new Valve3Exception("Illegal minimum/maximum frequencies.");
+		}
+	}
+	
+	private void plotWaveform()
+	{
+		SliceWaveRenderer wr = new SliceWaveRenderer();
 		wr.setRemoveBias(removeBias);
 		wr.setLocation(component.getBoxX(), component.getBoxY(), component.getBoxWidth(), component.getBoxHeight());
 		wr.setWave(wave);
-		double end = component.getEndTime();
-		double start = component.getStartTime(end);
-		wr.setViewTimes(start, end);
+		wr.setViewTimes(startTime, endTime);
 		double bias = 0;
 		if (removeBias)
-			bias = w.mean();
-		wr.setMinY(w.min() - bias);
-		wr.setMaxY(w.max() - bias);
+			bias = wave.mean();
+		wr.setMinY(wave.min() - bias);
+		wr.setMaxY(wave.max() - bias);
 		wr.update();
 		component.setTranslation(wr.getDefaultTranslation(v3Plot.getPlot().getHeight()));
 		component.setTranslationType("ty");
 		v3Plot.getPlot().addRenderer(wr);
 	}
 	
-	/*
-	private void plotSpectra(Valve3Plot v3Plot, PlotComponent component, Wave w)
+	private void plotSpectra()
 	{
-//		double minFreq = 0;
-//		double maxFreq = 15.0;
-		double minFreq = Double.parseDouble(component.get("spminf"));
-		double maxFreq = Double.parseDouble(component.get("spmaxf"));
-		boolean logFreq = false;
-		double[][] data = w.fft();
+		double[][] data = wave.fft();
 		data = FFT.halve(data);
-		FFT.toPowerFreq(data, w.getSamplingRate(), logFreq);
+		boolean logFreq = false;
+		FFT.toPowerFreq(data, wave.getSamplingRate(), logFreq);
 		double maxPower = -1E300;
 		for (int i = 2; i < data.length; i++)
 		{
@@ -84,19 +214,16 @@ public class WavePlotter extends Plotter
 		v3Plot.getPlot().addRenderer(dr); 	
 	}
 	
-	private void plotSpectrogram(Valve3Plot v3Plot, PlotComponent component, Wave w)
+	private void plotSpectrogram()
 	{
-		Wave wave = new Wave(w);
 		SpectrogramRenderer sr = new SpectrogramRenderer(wave);
 		//plot.setFrameRendererLocation(sr);
 		sr.setLocation(component.getBoxX(), component.getBoxY(), component.getBoxWidth(), component.getBoxHeight());
 		sr.setOverlap(0);
-//		sr.setHTicks(plot.getDefaultXTicks());
-//		sr.setVTicks(plot.getDefaultYTicks());
-		sr.setViewStartTime(wave.getStartTime());
-		sr.setViewEndTime(wave.getEndTime());
-		sr.setMinFreq(0);
-		sr.setMaxFreq(15);
+		sr.setViewStartTime(startTime);
+		sr.setViewEndTime(endTime);
+		sr.setMinFreq(minFreq);
+		sr.setMaxFreq(maxFreq);
 		//sr.setFftSize();
 		//sr.setTimeZoneOffset(Valve.getTimeZoneAdj());
 		sr.update(0);
@@ -109,70 +236,30 @@ public class WavePlotter extends Plotter
 		component.setTranslationType("ty");
 		v3Plot.getPlot().addRenderer(sr);
 	}
-	*/
-	public void plot(Valve3Plot v3Plot, PlotComponent component)
+
+	public void plot(Valve3Plot v3p, PlotComponent comp) throws Valve3Exception
 	{
-		v3Plot.setFilename(PlotHandler.getRandomFilename());
-		double end = component.getEndTime();
-		double start = component.getStartTime(end);
-		if (Double.isNaN(start) || Double.isNaN(end))
-		{
-			// return an error
-			return;
-		}
-		
-		String channel  = component.get("ch");
-		
-		HashMap<String, String> params = new HashMap<String, String>();
-		params.put("source", vdxSource);
-		params.put("selector", channel);
-		params.put("st", Double.toString(start));
-		params.put("et", Double.toString(end));
-//		SampledWave sw = (SampledWave)dataSource.getData(params);
-		
-		Pool<VDXClient> pool = Valve3.getInstance().getDataHandler().getVDXClient(vdxClient);
-		VDXClient client = pool.checkout();
-		Wave sw = (Wave)client.getData(params);
-		pool.checkin(client);
-		
-		// filter
-		String ft = component.get("ftype");
-		if (ft != null || ft.equals("N"))
-		{
-			Butterworth bw = new Butterworth();
-			if (ft.equals("L"))
-			{
-				double maxHz = Double.parseDouble(component.get("fmaxhz"));
-				bw.set(Butterworth.LOWPASS, 4, sw.getSamplingRate(), maxHz, 0);
-				sw.filter(bw, true);
-			}
-			else if (ft.equals("H"))
-			{
-				double minHz = Double.parseDouble(component.get("fminhz"));
-				bw.set(Butterworth.HIGHPASS, 4, sw.getSamplingRate(), minHz, 0);
-				sw.filter(bw, true);
-			}
-			else if (ft.equals("B"))
-			{
-				double minHz = Double.parseDouble(component.get("fminhz"));
-				double maxHz = Double.parseDouble(component.get("fmaxhz"));
-				bw.set(Butterworth.BANDPASS, 4, sw.getSamplingRate(), minHz, maxHz);
-				sw.filter(bw, true);
-			}
-		}
+		v3Plot = v3p;
+		component = comp;
+		getInputs();
+		getData();
 		Plot plot = v3Plot.getPlot();
 		plot.setBackgroundColor(Color.white);
 		
-		String outputType = component.get("type");
-		if (outputType.equals("wf"))
-			plotWaveform(v3Plot, component, sw);
-		/*
-		else if (outputType.equals("sp"))
-			plotSpectra(v3Plot, component, sw);
-		else if (outputType.equals("sg"))
-			plotSpectrogram(v3Plot, component, sw);
-			*/
-		
+		switch(plotType)
+		{
+			case WAVEFORM:
+				plotWaveform();
+				break;
+			case SPECTRA:
+				plotSpectra();
+				break;
+			case SPECTROGRAM:
+				plotSpectrogram();
+				break;
+		}
+
+		v3Plot.setFilename(PlotHandler.getRandomFilename());
 		plot.writePNG(Valve3.getInstance().getApplicationPath() + File.separatorChar + v3Plot.getFilename());
 		v3Plot.addComponent(component);
 		String ch = channel.replace('$', ' ').replace('_', ' ');
