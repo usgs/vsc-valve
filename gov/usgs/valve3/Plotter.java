036e9947163b98cc176a69b242936fccf983c659
Dan Cervelli <dcervelli@mycompany.com>
2005-10-13 20:34:32 +0000

    Added plotterConfig.
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3/trunk@275 b23e0666-5c4d-0410-a39a-a125d005d78a

--- gov/usgs/valve3/Plotter.java
+++ gov/usgs/valve3/Plotter.java
@@ -1,9 +1,13 @@
 package gov.usgs.valve3;
 
+import gov.usgs.util.ConfigFile;
 import gov.usgs.valve3.result.Valve3Plot;
 
 /**
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/08/28 19:00:46  dcervelli
+ * plot() now throws exceptions.
+ *
  * Revision 1.1  2005/08/26 20:41:31  dcervelli
  * Initial avosouth commit.
  *
@@ -14,6 +18,8 @@ abstract public class Plotter
 	protected String vdxClient;
 	protected String vdxSource;
 	
+	protected ConfigFile plotterConfig;
+	
 	public void setVDXClient(String c)
 	{
 		vdxClient = c;
@@ -22,6 +28,11 @@ abstract public class Plotter
 	public void setVDXSource(String s)
 	{
 		vdxSource = s;
+	}
+	
+	public void setPlotterConfig(ConfigFile cf)
+	{
+		plotterConfig = cf;
 	}
 	
 	abstract public void plot(Valve3Plot plot, PlotComponent component) throws Valve3Exception;
