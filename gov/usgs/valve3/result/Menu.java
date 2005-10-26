464ee849e9f63c2d0ec402bb3d86f88388860069
Tom Parker <tparker-usgs@users.noreply.github.com>
2005-10-26 18:43:40 +0000

    bug Id #68
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3/trunk@324 b23e0666-5c4d-0410-a39a-a125d005d78a

--- gov/usgs/valve3/result/Menu.java
+++ gov/usgs/valve3/result/Menu.java
@@ -10,6 +10,9 @@ import java.util.List;
 /**
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/10/26 17:59:15  tparker
+ * Add timezone for Bug #68
+ *
  * Revision 1.1  2005/08/26 20:41:31  dcervelli
  * Initial avosouth commit.
  *
@@ -31,7 +34,7 @@ public class Menu extends Result
 		sb.append("\t\t<title>" + Valve3.getInstance().getInstallationTitle() + "</title>\n");
 		sb.append("\t\t<administrator><![CDATA[" + Valve3.getInstance().getAdministrator() + "]]></administrator>\n");
 		sb.append("\t\t<administrator-email><![CDATA[" + Valve3.getInstance().getAdministratorEmail() + "]]></administrator-email>\n");
-		sb.append("\t\t<timeZoneAbbr><![CDATA[" + Valve3.getInstance().getTimeZoneAbbr() + "]]></timeZoneAbbr>\n");
+		sb.append("\t\t<timeZoneAbbr>" + Valve3.getInstance().getTimeZoneAbbr() + "</timeZoneAbbr>\n");
 		sb.append("\t\t<version>" + Valve3.VERSION + ", " + Valve3.BUILD_DATE + "</version>\n");
 		sb.append("\t\t<sections>\n");
 		
