7da464d8cfe86771ca1be8acf6a9edd8d7c86596
Tom Parker <tparker-usgs@users.noreply.github.com>
2006-08-29 00:03:17 +0000

    Initial NWIS commit
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3/trunk@832 b23e0666-5c4d-0410-a39a-a125d005d78a

---
+++ gov/usgs/valve3/plotter/NWISPlotter.java
@@ -0,0 +1,258 @@
+package gov.usgs.valve3.plotter;
+
+import gov.usgs.plot.AxisRenderer;
+import gov.usgs.plot.MatrixRenderer;
+import gov.usgs.plot.Plot;
+import gov.usgs.plot.ShapeRenderer;
+import gov.usgs.plot.SmartTick;
+import gov.usgs.util.Pool;
+import gov.usgs.valve3.PlotComponent;
+import gov.usgs.valve3.PlotHandler;
+import gov.usgs.valve3.Plotter;
+import gov.usgs.valve3.Valve3;
+import gov.usgs.valve3.Valve3Exception;
+import gov.usgs.valve3.result.GenericMenu;
+import gov.usgs.valve3.result.Valve3Plot;
+import gov.usgs.vdx.client.VDXClient;
+import gov.usgs.vdx.data.GenericDataMatrix;
+import gov.usgs.vdx.data.generic.GenericColumn;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 
+ * $Log: not supported by cvs2svn $
+ *
+ * @author Tom Parker
+ */
+public class NWISPlotter extends Plotter
+{
+	private Valve3Plot v3Plot;
+	private PlotComponent component;
+	private double startTime;
+	private double endTime;
+	private GenericDataMatrix data;
+	private String channel;
+	private String channelName = "";
+	private GenericMenu menu;
+	
+	private String leftUnit;
+	private List<GenericColumn> leftColumns;
+	private String rightUnit;
+	private List<GenericColumn> rightColumns;
+
+	public NWISPlotter()
+	{}
+	
+	private void getData() throws Valve3Exception
+	{
+		HashMap<String, String> params = new HashMap<String, String>();
+		Pool<VDXClient> pool = Valve3.getInstance().getDataHandler().getVDXClient(vdxClient);
+		VDXClient client = pool.checkout();
+		params.put("source", vdxSource);
+		params.put("action", "data");
+		params.put("cid", channel);
+		params.put("st", Double.toString(startTime));
+		params.put("et", Double.toString(endTime));
+		params.put("selectedTypes", component.get("selectedTypes"));
+
+		data = (GenericDataMatrix)client.getBinaryData(params);
+		pool.checkin(client);
+		
+		if (data == null || data.rows() == 0)
+			throw new Valve3Exception("No data.");
+		data.adjustTime(Valve3.getInstance().getTimeZoneOffset() * 60 * 60);
+		startTime += Valve3.getInstance().getTimeZoneOffset() * 60 * 60;
+		endTime += Valve3.getInstance().getTimeZoneOffset() * 60 * 60;
+	}
+	
+	private void getInputs() throws Valve3Exception
+	{
+		
+		channel = component.get("ch");
+		if (channel == null || channel.length() <= 0)
+			throw new Valve3Exception("Illegal channel.");
+		
+		endTime = component.getEndTime();
+		if (Double.isNaN(endTime))
+			throw new Valve3Exception("Illegal end time.");
+		startTime = component.getStartTime(endTime);
+		if (Double.isNaN(startTime))
+			throw new Valve3Exception("Illegal start time.");
+		
+		String[] types = component.get("dataTypes").split("\\$");
+		String[] selectedTypes = component.get("selectedTypes").split(":");
+		int c = 0;
+		for (int i = 0; i<types.length; i++)
+		{
+			int type = Integer.parseInt(types[i].split(":")[0]);
+			String description = types[i].split(":")[1];
+			boolean sel = false;
+			
+			for (int j = 0; j<selectedTypes.length; j++)
+				if (Integer.parseInt(selectedTypes[j]) == type)
+					sel = true;
+			
+			if (!sel)
+				continue;		
+			
+			String columnSpec = c++ + ":" + description + ":" + description + ":" + description + ":T";
+			
+			GenericColumn col = new GenericColumn(columnSpec);
+			
+			if (leftUnit != null && leftUnit.equals(col.unit))
+				leftColumns.add(col);
+			else if (rightUnit != null && rightUnit.equals(col.unit))
+				rightColumns.add(col);
+			else if (leftUnit == null)
+			{
+				leftUnit = col.unit;
+				leftColumns = new ArrayList<GenericColumn>();
+				leftColumns.add(col);
+			}
+			else if (rightUnit == null)
+			{
+				rightUnit = col.unit;
+				rightColumns = new ArrayList<GenericColumn>();
+				rightColumns.add(col);
+			}
+			else
+				throw new Valve3Exception("Too many different units.");
+		}
+		
+		if (leftUnit == null && rightUnit == null)
+			throw new Valve3Exception("Nothing to plot.");
+		
+		if (rightUnit != null)
+		{
+			int minRight = Integer.MAX_VALUE;
+			for (GenericColumn col : rightColumns)
+				minRight = Math.min(minRight, col.index);
+			
+			int minLeft = Integer.MAX_VALUE;
+			for (GenericColumn col : leftColumns)
+				minLeft = Math.min(minLeft, col.index);
+			
+			if (minLeft > minRight)
+			{
+				String tempUnit = leftUnit;
+				List<GenericColumn> tempColumns = leftColumns;
+				leftUnit = rightUnit;
+				leftColumns = rightColumns;
+				rightUnit = tempUnit;
+				rightColumns = tempColumns;
+			}
+		}
+	}
+	
+	private MatrixRenderer getLeftMatrixRenderer()
+	{
+		MatrixRenderer mr = new MatrixRenderer(data.getData());
+		mr.setLocation(component.getBoxX(), component.getBoxY(), component.getBoxWidth(), component.getBoxHeight());
+		
+		double max = -1E300;
+		double min = 1E300;
+		
+		mr.setAllVisible(false);
+		for (GenericColumn col : leftColumns)
+		{
+			mr.setVisible(col.index, true);
+			max = Math.max(max, data.max(col.index + 1));
+			min = Math.min(min, data.min(col.index + 1));
+		}
+		
+		mr.setExtents(startTime, endTime, min, max);		
+		mr.createDefaultAxis(8, 8, false, true);
+		mr.createDefaultLineRenderers();
+		mr.setXAxisToTime(8);
+		mr.getAxis().setLeftLabelAsText(leftColumns.get(0).description);
+		mr.getAxis().setBottomLabelAsText("Time");
+		return mr;
+	}
+	
+	private MatrixRenderer getRightMatrixRenderer()
+	{
+		if (rightUnit == null)
+			return null;
+		
+		MatrixRenderer mr = new MatrixRenderer(data.getData());
+		mr.setLocation(component.getBoxX(), component.getBoxY(), component.getBoxWidth(), component.getBoxHeight());
+		
+		double max = -1E300;
+		double min = 1E300;
+		
+		mr.setAllVisible(false);
+		for (GenericColumn col : rightColumns)
+		{
+			mr.setVisible(col.index, true);
+			max = Math.max(max, data.max(col.index + 1));
+			min = Math.min(min, data.min(col.index + 1));
+		}
+		
+		mr.setExtents(startTime, endTime, min, max);
+		AxisRenderer ar = new AxisRenderer(mr);
+		ar.createRightTickLabels(SmartTick.autoTick(min, max, 8, false), null);
+		mr.setAxis(ar);
+		mr.createDefaultLineRenderers();
+		ShapeRenderer[] r = mr.getLineRenderers();
+		r[1].color = Color.red;
+
+		mr.getAxis().setRightLabelAsText(rightColumns.get(0).description);
+		return mr;
+	}
+	
+	public void plotData()
+	{
+		MatrixRenderer leftMR = getLeftMatrixRenderer();
+		MatrixRenderer rightMR = getRightMatrixRenderer();
+		v3Plot.getPlot().addRenderer(leftMR);
+		if (rightMR != null)
+			v3Plot.getPlot().addRenderer(rightMR);
+		
+		component.setTranslation(leftMR.getDefaultTranslation(v3Plot.getPlot().getHeight()));
+		component.setTranslationType("ty");
+	}
+	
+	public void plot(Valve3Plot v3p, PlotComponent comp) throws Valve3Exception
+	{
+				
+		v3Plot = v3p;
+		component = comp;
+		getInputs();		
+		getData();
+				
+		Plot plot = v3Plot.getPlot();
+		plot.setBackgroundColor(Color.white);
+		
+		plotData();
+
+		v3Plot.addComponent(component);
+		v3Plot.setTitle(Valve3.getInstance().getMenuHandler().getItem(vdxSource).name + ":" + comp.get("ch"));
+		v3Plot.setFilename(PlotHandler.getRandomFilename());
+		plot.writePNG(Valve3.getInstance().getApplicationPath() + File.separatorChar + v3Plot.getFilename());
+	}
+	
+	public String toCSV(PlotComponent comp) throws Valve3Exception
+	{
+		
+		HashMap<String, String> params = new HashMap<String, String>();
+		Pool<VDXClient> pool = Valve3.getInstance().getDataHandler().getVDXClient(vdxClient);
+		VDXClient client = pool.checkout();
+		
+		params.put("source", vdxSource);
+		params.put("action", "genericMenu");
+		menu = new GenericMenu(client.getTextData(params));
+		pool.checkin(client);
+		
+		component = comp;
+		getInputs();
+		getData();
+		
+		return data.getData().toString();
+	}
+
+}
