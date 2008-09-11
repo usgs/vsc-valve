d55076251238d3f7ed5197c4b8523307489b68bc
Tom Parker <tparker-usgs@users.noreply.github.com>
2008-09-11 18:14:20 +0000

    Make UI for EW RSAM data dynamic
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3Web/trunk@1236 b23e0666-5c4d-0410-a39a-a125d005d78a

--- web/menu/ewrsammenu.js
+++ web/menu/ewrsammenu.js
@@ -7,4 +7,29 @@ create_ewrsammenu = function(menu)
 	menu.boxName = "ewrsamBox";
 	menu.selector = "selector:ch";
 	menu.timeShortcuts = new Array("-1m", "-6m", "-1y", "-2y", "-4y");
+	
+	// make this work
+	menu.initialize = function()
+	{
+		Menu.prototype.initialize.call(this);
+		loadXML(this.id + " generic menu description", "valve3.jsp?a=data&da=ewRsamMenu&src=" + this.id, 
+			function(req)
+			{
+				var xml = req.responseXML;				
+				var types = xml.getElementsByTagName("dataTypes")[0].firstChild.data;
+				if (types == "[VALUES]")
+				{
+					document.getElementById(menu.id + "_mv").checked = "true";
+				}
+				else if (types == "[EVENTS]")
+				{
+					document.getElementById(menu.id + "_cnts").checked = "true";
+				}
+				else
+				{
+					document.getElementById(menu.id + "_outputTypeBox").style.display = "block";
+				}
+					
+			});
+	}
 }
