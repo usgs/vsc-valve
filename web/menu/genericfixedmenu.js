6e196a9a7e376023a94d674cccbbf86370d4b10a
Loren Antolik <loren@mycompany.com>
2009-03-17 23:49:04 +0000

    rename create_genericmenu to create_genericfixedmenu.  cleanup
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3Web/trunk@1388 b23e0666-5c4d-0410-a39a-a125d005d78a

--- web/menu/genericfixedmenu.js
+++ web/menu/genericfixedmenu.js
@@ -1,34 +1,28 @@
-// $Id: genericmenu.js,v 1.1 2005-10-20 05:08:40 dcervelli Exp $
-
-create_genericmenu = function(menu)
-{
-	menu.allowChannelMap = true;
-	menu.formName = "genericForm";
-	menu.boxName = "genericBox";
-	menu.selector = "selector:ch";
-	menu.timeShortcuts = new Array("-1h", "-2h", "-6h", "-12h", "-24h", "-2d", "-3d", "-1w", "-2w", "-1m");
+create_genericfixedmenu = function(menu) {
+	menu.allowChannelMap	= true;
+	menu.formName			= "genericForm";
+	menu.boxName			= "genericBox";
+	menu.selector			= "selector:ch";
+	menu.timeShortcuts		= new Array("-1h", "-2h", "-6h", "-12h", "-24h", "-2d", "-3d", "-1w", "-2w", "-1m");
 	
 	// TODO: labelize checkboxes
-	menu.initialize = function()
-	{
+	menu.initialize = function() {
 		Menu.prototype.initialize.call(this);
 		loadXML(this.id + " generic menu description", "valve3.jsp?a=data&da=genericMenu&src=" + this.id, 
-			function(req)
-			{
-				var xml = req.responseXML;
-				
+			function(req) {
+				var xml = req.responseXML;				
 				var ip = document.getElementById(menu.id + "_insertionPoint");
 				var cols = xml.getElementsByTagName("column");
-				for (var i = 0; i < cols.length; i++)
-				{
+				for (var i = 0; i < cols.length; i++) {
 					var p = document.createElement('p');					
 					var col = cols[i].firstChild.data.split(":");
 					var label = " " + col[2] + " (" + col[3] + ")";
 					var cb = document.createElement('input');
 					cb.type = 'checkbox';
 					cb.id = menu.id + "_" + col[1];
-					if (col[4] == "T")
+					if (col[4] == "T") {
 						cb.checked = "checked";
+					}
 					cb.name = col[1];
 					p.appendChild(cb);
 					var tn = document.createTextNode(label);
