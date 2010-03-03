c1eaa8b1e4a5bb4eb4015b330df65a474a3cd4e1
Loren Antolik <loren@mycompany.com>
2010-03-03 03:59:32 +0000

    Updated version to 3.3.0.
    git-svn-id: http://avosouth.wr.usgs.gov/vhpsvn/Valve3/branches/Valve3.3.0@1681 b23e0666-5c4d-0410-a39a-a125d005d78a

--- gov/usgs/valve3/Valve3.java
+++ gov/usgs/valve3/Valve3.java
@@ -55,8 +55,8 @@ import javax.servlet.ServletContextListener;
  */
 public class Valve3 implements ServletContextListener
 {
-	public static final String VERSION = "3.2.7";
-	public static final String BUILD_DATE = "2009-11-09";
+	public static final String VERSION = "3.3.0";
+	public static final String BUILD_DATE = "2010-02-25";
 	
 	public static final String CONFIG_PATH = File.separator + "WEB-INF" + File.separator + "config" + File.separator;
 	private static final String CONFIG_FILE = "valve3.config";
